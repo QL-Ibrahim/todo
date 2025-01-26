6cf775fee2b329db0c14ec272b9e9899f8e35468
Ibrahim <Ibrahim@quokkalabs.com>
2025-01-26 21:24:34 +0530

    testing quote

--- src/app/tasks/[taskId]/page.tsx
+++ src/app/tasks/[taskId]/page.tsx
@@ -21,7 +21,7 @@ const TaskDetailsPage = ({ params }: { params: { taskId: string } }) => {
 
   const fetchTask = async () => {
     const docRef = doc(db, 'tasks', taskId);
-    const docSnap = getDoc(docRef);
+    const docSnap = await getDoc(docRef);
     if (docSnap.exists()) {
       const taskData = docSnap.data();
       setTask({
